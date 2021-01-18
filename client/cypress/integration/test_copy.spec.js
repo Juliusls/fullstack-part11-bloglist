0858364931d42a7de19ac883d0da08e025b81ea9
Julius <julius.lubys@gmail.com>
2021-01-18 14:19:58 +0200

    fixing initial deploypment pipeline

--- client/cypress/integration/test_copy.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Blog app', function() {
-    it('page can be visited', function() {
-        cy.visit('http://localhost:3001')
-        cy.login({ username: 'Julius', password: 'mypassword' })
-        cy.contains('Julius Lubys logged in')
-    })
-})
